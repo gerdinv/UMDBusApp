26e65fc76b81ee85574f24063455e3e4f3db9615
Gerdin <gerdinventura2@gmail.com>
2021-09-30 23:46:37 -0400

    Initial Commit

---
+++ UMDBusAppTests/UMDBusAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  UMDBusAppTests.swift
+//  UMDBusAppTests
+//
+//  Created by Gerdin Ventura on 9/30/21.
+//
+
+import XCTest
+@testable import UMDBusApp
+
+class UMDBusAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
